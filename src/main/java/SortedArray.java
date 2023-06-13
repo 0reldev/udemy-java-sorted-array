790b437aae789dfd0b61ae681bd9534e041fb4db
Aurélien Lonni <aurelien.lonni@gmail.com>
2023-06-13 16:14:03 +0200

    Creation of sortIntegers method

--- src/main/java/SortedArray.java
+++ src/main/java/SortedArray.java
@@ -20,4 +20,15 @@ public class SortedArray {
             System.out.printf("Element %d contents %d%n", i, array[i]);
         }
     }
+
+    public static int[] sortIntegers(int[] array) {
+        int[] sortedArray = Arrays.copyOf(array, array.length);
+        Arrays.sort(sortedArray);
+        int[] reversedArray = new int[sortedArray.length];
+        for (int i = 0 ; i < sortedArray.length ; i++) {
+            reversedArray[i] = sortedArray[sortedArray.length - i - 1];
+        }
+        System.out.println("Array's sorting. New array:\n" + Arrays.toString(reversedArray) + "\n");
+        return reversedArray;
+    }
 }
